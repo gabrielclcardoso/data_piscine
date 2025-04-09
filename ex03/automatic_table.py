d59d5fa60391fd91bdced05528185159a45db4a6
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-04-09 10:53:16 -0300

    feat: initial structure defined

--- ex03/automatic_table.py
+++ ex03/automatic_table.py
@@ -1,67 +1,75 @@
-# import psycopg
+import psycopg
 from pathlib import Path
 
 
 def main():
-    iterate_directory("subject")
-    # with psycopg.connect("host=localhost dbname=piscineds user=gcorreia " +
-    #                      "password=mysecretpassword") as conn:
-    #     with conn.cursor() as cur:
-    #         create_table(conn, cur)
-    #         copy_from_csv(conn, cur)
+    connect_string = """
+    host=localhost
+    dbname=piscineds
+    user=gcorreia
+    """
+    files = get_csv_files("subject/customer")
+
+    with psycopg.connect(connect_string) as conn:
+        with conn.cursor() as cur:
+            try:
+                create_type(cur)
+            except Exception as e:
+                print(f"Error: {e}")
+                conn.rollback()
+            for f in files:
+                try:
+                    create_table(cur, f)
+                    copy_content(cur, f)
+                except Exception as e:
+                    print(f"Error at file {f.name}: {e}")
+                    conn.rollback()
 
 
-def iterate_directory(path: str) -> None:
+def create_type(cur: psycopg.Cursor):
+    """Creates the TYPE event on the database"""
+    events = ('cart', 'view', 'remove_from_cart', 'purchase')
+
+    cur.execute(f"CREATE TYPE event as ENUM {events};")
+
+
+def get_csv_files(path: str) -> list[Path]:
+    """Returns a list of csv files in the given directory"""
+
     dir = Path(path)
-    csv_files = list(dir.glob("**/*.csv"))
-    for f in csv_files:
-        print(f.name[:-4])
+    return list(dir.glob("**/*.csv"))
 
-# def create_table(conn, cur):
-#     """Creates the table data_2022_oct if it doesn't exist"""
-#
-#     events = ('cart', 'view', 'remove_from_cart', 'purchase')
-#
-#     try:
-#         cur.execute(f"CREATE TYPE event as ENUM {events};")
-#     except psycopg.Error as e:
-#         print(f"Error: {e}")
-#         conn.rollback()
-#
-#     try:
-#         cur.execute("""
-#             CREATE TABLE data_2022_oct (
-#                 event_time timestamp with time zone,
-#                 event_type event,
-#                 product_id integer,
-#                 price real,
-#                 user_id bigint,
-#                 user_session char(36)
-#             );
-#         """)
-#     except psycopg.Error as e:
-#         print(f"Error: {e}")
-#         conn.rollback()
-#
-#
-# def copy_from_csv(conn, cur):
-#     """Copies data in the subject/customer/data_2022_oct.csv file"""
-#
-#     sql_copy = """
-#         COPY data_2022_oct
-#         FROM STDIN
-#         WITH (
-#             FORMAT CSV,
-#             HEADER TRUE
-#         );
-#     """
-#
-#     try:
-#         with open("subject/customer/data_2022_oct.csv") as f:
-#             with cur.copy(sql_copy) as copy:
-#                 copy.write(f.read())
-#     except Exception as e:
-#         print(f"Error: {e}")
+
+def create_table(cur: psycopg.Cursor, file: Path):
+    """Creates the table acording to the file name"""
+
+    cur.execute(f"""
+        CREATE TABLE {file.name[-4]} (
+            event_time timestamp with time zone,
+            event_type event,
+            product_id integer,
+            price real,
+            user_id bigint,
+            user_session char(36)
+        );
+    """)
+
+
+def copy_content(cur: psycopg.Cursor, file: Path):
+    """Copies data from the file to it's respective table"""
+
+    sql_copy = f"""
+        COPY {file.name[-4]}
+        FROM STDIN
+        WITH (
+            FORMAT CSV,
+            HEADER TRUE
+        );
+    """
+
+    with file.open() as f:
+        with cur.copy(sql_copy) as copy:
+            copy.write(f.read())
 
 
 if __name__ == "__main__":
