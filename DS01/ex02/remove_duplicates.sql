9fbd5ad42716f33018e938560d0c060fbf589928
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-04-26 13:52:18 -0300

    feat: ex02 done

---
+++ DS01/ex02/remove_duplicates.sql
@@ -0,0 +1,31 @@
+WITH ComparableRows AS (
+    SELECT
+        id, event_time, event_type, product_id, price, user_id, user_session,
+        LAG(event_time, 1, NULL) OVER (ORDER BY event_time) AS previous_etime,
+        LAG(event_type, 1, NULL) OVER (ORDER BY event_time) AS previous_etype,
+        LAG(product_id, 1, NULL) OVER (ORDER BY event_time) AS previous_pid,
+        LAG(price, 1, NULL) OVER (ORDER BY event_time) AS previous_price,
+        LAG(user_id, 1, NULL) OVER (ORDER BY event_time) AS previous_uid,
+        LAG(user_session, 1, NULL) OVER (ORDER BY event_time) AS previous_session
+    FROM
+        customers
+),
+Duplicates AS (
+    SELECT
+        id
+    FROM
+        ComparableRows
+    WHERE
+        previous_etime IS NOT NULL
+        AND ABS(EXTRACT(EPOCH FROM (event_time - previous_etime))) <= 1
+        AND event_type = previous_etype
+        AND product_id = previous_pid
+        AND price = previous_price
+        AND user_id = previous_uid
+        AND user_session = previous_session
+)
+DELETE FROM customers
+WHERE id IN (SELECT id FROM Duplicates);
+
+-- ID 130 and 131 are duplicates and ID 10 and 11 aswell
+-- time psql -h localhost -U gcorreia -d piscineds -a -f remove_duplicates.sql
