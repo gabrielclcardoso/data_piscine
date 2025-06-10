7a0f1253f8daebe0382527cd7cbc12e583417f80
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-06-10 15:42:17 -0300

    fix: fixed plot formatting

--- DS03/ex00/Histogram.py
+++ DS03/ex00/Histogram.py
@@ -8,6 +8,8 @@ def main():
     try:
         data = pd.read_csv(FILE)
         data.hist()
+        plt.subplots_adjust(hspace=0.55, top=0.95,
+                            bottom=0.05, left=0.02, right=0.98)
         plt.show()
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
