b64e5e5ee4edfa81561438ff81e1f8bcb3779333
gabrielclcardoso <gabrielclcardoso@gmail.com>
2025-08-23 14:24:03 -0300

    feat: ex04 done

--- DS04/ex04/Tree.py
+++ DS04/ex04/Tree.py
@@ -3,9 +3,10 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
 from sklearn import tree
 from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
 
 
 def main():
@@ -20,53 +21,45 @@ def main():
     features = train_data.drop(columns="knight")
     targets = train_data["knight"]
 
-    decision_tree = train_decision_tree(features, targets)
-    predictions = run_predictions(decision_tree, test_data)
+    clf = train_decision_tree(features, targets)
+    run_predictions(clf, test_data)
 
-    try:
-        print_f1_score(predictions)
-    except Exception as e:
-        print(f"{type(e).__name__}: {e}")
-        return 1
-
-    # plot_tree(decision_tree, features)
+    plot_tree(clf, features)
 
 
 def train_decision_tree(features, targets):
-    """Trains a decision tree based on the features and targets provided
-    and prints it's cross validation score2"""
+    """Trains a decision tree based on the best parameters found with a
+    GridSearchCV"""
 
-    decision_tree = DecisionTreeClassifier(random_state=0)
-    decision_tree.fit(features, targets)
+    param_grid = {
+        'max_depth': [3, 4, 5, 6, 7, 8],
+        'min_samples_leaf': [5, 10, 15, 20],
+        'min_samples_split': [10, 20, 30, 40],
+        'criterion': ['gini', 'entropy']
+    }
+    scorer = make_scorer(f1_score, pos_label='Jedi')
 
-    score = cross_val_score(decision_tree, features, targets, cv=10)
-    print(f"Cross Validation scores for decision tree:\n{score}")
+    dt = DecisionTreeClassifier(random_state=0)
+    gs = GridSearchCV(estimator=dt, param_grid=param_grid, cv=5,
+                      n_jobs=-1, verbose=1, scoring=scorer)
+    gs.fit(features, targets)
 
-    return decision_tree
+    best_tree = gs.best_estimator_
+    print(
+        f"Mean cross validated f1-score of the chosen tree: {gs.best_score_}")
+    print(f"parameters chosen = {gs.best_params_}")
+
+    return best_tree
 
 
-def run_predictions(decision_tree, test_data):
+def run_predictions(clf, test_data):
     """Runs the decision tree on the testing data and writes the reults to a
-    prediction.txt file"""
+    Tree.txt file"""
 
-    predictions = decision_tree.predict(test_data)
+    predictions = clf.predict(test_data)
     content = '\n'.join(predictions)
-    with open("prediction.txt", mode='w') as f:
+    with open("Tree.txt", mode='w') as f:
         f.write(content)
-
-    return predictions
-
-
-def print_f1_score(predictions):
-    """Gets the correct predictions from a truth.txt file and prints the
-    f1 score of the given predictions"""
-
-    with open("truth.txt") as f:
-        truth = [line.strip() for line in f]
-
-    print(f"{len(truth)} vs {len(predictions[:len(truth)])}")
-    score = f1_score(truth, predictions[:len(truth)], pos_label="Sith")
-    print(f"f1 score = {score:.2f}")
 
 
 def plot_tree(clsifier, features):
